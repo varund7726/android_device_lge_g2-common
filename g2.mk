d154afa62f0eb0affb082670c85354172f35f7f3
Rashed Abdel-Tawab <rashed@linux.com>
2015-01-08 20:06:56 -0500

    configs: Update media and sensor configuration
    -Update media codec capabilities and profiles
    -Add Qualcomm audio effects (currently disabled)
    -Add sensor configuration
    -Import missing telephony codecs
    
    Change-Id: I8d9282bbf60fb82824e2de86a45fb2bcee15f2e5

--- g2.mk
+++ g2.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2011 The Android Open-Source Project
+# Copyright (C) 2013-2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -42,12 +42,17 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fstab.g2:root/fstab.g2
 endif
 
+# Media and sensors
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/configs/sensor_def_common.conf:system/etc/sensor_def_common.conf \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
 
@@ -199,9 +204,6 @@ PRODUCT_PACKAGES += \
         libqcompostprocbundle \
         libqcomvoiceprocessing \
         libqcomvoiceprocessingdescriptors
-
-PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
