a85f30f2df49fdd845de632741a8e3c67bcbd890
Rashed Abdel-Tawab <rashed@linux.com>
2015-01-14 18:42:42 -0500

    g2: Oops...
    Change-Id: Ia4202b00efe4887c6ea197d51bcfa1e1071c92b6

--- g2.mk
+++ g2.mk
@@ -32,15 +32,15 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Init
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init.g2.usb.rc:root/init.g2.usb.rc \
-    $(LOCAL_PATH)/ueventd.g2.rc:root/ueventd.g2.rc
+    $(LOCAL_PATH)/rootdir/etc/init.g2.usb.rc:root/init.g2.usb.rc \
+    $(LOCAL_PATH)/rootdir/etc/ueventd.g2.rc:root/ueventd.g2.rc
 
 # F320 requires different versions of these for SD card access
 # so use these only if we're NOT building F320
 ifneq ($(TARGET_DEVICE),f320)
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init.g2.rc:root/init.g2.rc \
-    $(LOCAL_PATH)/fstab.g2:root/fstab.g2
+    $(LOCAL_PATH)/rootdir/etc/init.g2.rc:root/init.g2.rc \
+    $(LOCAL_PATH)/rootdir/etc/fstab.g2:root/fstab.g2
 endif
 
 # Sensors
@@ -255,9 +255,9 @@ PRODUCT_PACKAGES += \
 #    init.g2.usb.rc \
 #    init.g2.usb.sh \
 #    ueventd.g2.rc
-
-PRODUCT_PACKAGES += \
-    init.galbi.thermal_conf.sh
+#
+#PRODUCT_PACKAGES += \
+#    init.galbi.thermal_conf.sh
 
 # This hw ships locked, work around it with loki
 PRODUCT_PACKAGES += \
