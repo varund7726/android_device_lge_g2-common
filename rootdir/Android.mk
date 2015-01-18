25ea7f828b746efbe34c042855299a603f25e593
Rashed Abdel-Tawab <rashed@linux.com>
2015-01-17 20:55:03 -0500

    init: More sensor changes
    Change-Id: I0cfba002b4fe4068975baedb7fc71f18d7bac8b9

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -265,11 +265,11 @@ LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := init.galbi-sensor.sh
+LOCAL_MODULE            := init.galbi2.sensor.sh
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.galbi-sensor.sh
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES         := etc/init.galbi2.sensor.sh
+#LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
