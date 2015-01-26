0d2d90dece9e45ed60437eda2ecd765117ccc1e9
Rashed Abdel-Tawab <rashed@linux.com>
2015-01-25 21:38:46 -0500

    releasetools: execute panel detection before loki
    Change-Id: I3e0cb2584c79db3c961b03dee69772d25128f547

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -31,8 +31,8 @@ def FullOTA_InstallEnd(info):
   info.script.script = [cmd for cmd in info.script.script if not "show_progress(0.100000, 0);" in cmd]
   info.script.AppendExtra('package_extract_file("boot.img", "/tmp/boot.img");')
   info.script.Mount("/system")
-  info.script.AppendExtra('assert(run_program("/system/bin/loki.sh") == 0);')
-  info.script.AppendExtra('delete("/system/bin/loki.sh");')
   info.script.AppendExtra('assert(run_program("/system/bin/panel.sh") == 0);')
+  info.script.AppendExtra('assert(run_program("/system/bin/loki.sh") == 0);')
   info.script.AppendExtra('delete("/system/bin/panel.sh");')
+  info.script.AppendExtra('delete("/system/bin/loki.sh");')
   info.script.Unmount("/system")
