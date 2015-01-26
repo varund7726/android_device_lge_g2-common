923444be0e7050855aa3e8985223ee2aac20779c
Rashed Abdel-Tawab <rashed@linux.com>
2015-01-25 23:57:28 -0500

    releasetools: Cleanup panel detection script
    * Only change the part of the command line that sets the panel
      not the entire command line
    * Get rid of redundant code and just check /tmp/loki and /tmp
      at the same time instead of 2 different times
    
    Change-Id: Ic150be98cb59211caebf65dab1177d5dd8ea3a11

--- releasetools/panel.sh
+++ releasetools/panel.sh
@@ -1,6 +1,6 @@
 #!/sbin/sh
 #
-# 4.4 and beyond Kernel - Panel Detection - dr87
+# 4.4 and beyond Kernel - Panel Detection - dr87 & Rashed97
 #
 # Detect panel and swap as necessary 
 # lcd_maker_id is determined by get_panel_maker_id on the hardware and is always accurate
@@ -9,31 +9,20 @@
 #
 #	LCD_RENESAS_LGD = 0,
 #	LCD_RENESAS_JDI = 1
-
-# Search the two common locations to extract boot.img for loki
-# 
-# Search for boot.img in /tmp/loki
-
-lcdmaker=$(grep -c "lcd_maker_id=1" /proc/cmdline)
-if [ $lcdmaker == 1 ]; then
-	echo "JDI panel detected"
-	find /tmp/loki/boot.img -type f -exec sed -i 's/console=ttyHSL0,115200,n8 androidboot.hardware=g2 androidboot.bootdevice=msm_sdcc.1 user_debug=31 msm_rtb.filter=0x0 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd androidboot.selinux=permissive/console=ttyHSL0,115200,n8 androidboot.hardware=g2 androidboot.bootdevice=msm_sdcc.1 user_debug=31 msm_rtb.filter=0x0 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd androidboot.selinux=permissive/g' {} \;
-elif [ $lcdmaker == "0" ]; then
-	echo "LGD panel detected"
-	find /tmp/loki/boot.img -type f -exec sed -i 's/console=ttyHSL0,115200,n8 androidboot.hardware=g2 androidboot.bootdevice=msm_sdcc.1 user_debug=31 msm_rtb.filter=0x0 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd androidboot.selinux=permissive/console=ttyHSL0,115200,n8 androidboot.hardware=g2 androidboot.bootdevice=msm_sdcc.1 user_debug=31 msm_rtb.filter=0x0 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd androidboot.selinux=permissive/g' {} \;
-else
-	echo "lcd_maker_id doesn't exist. Something went wrong."
-fi
-
-# Search for boot.img in /tmp
+#
+# Some ROMs will place boot.img in /tmp instead of /tmp/loki
+# so support finding boot.img both locations
+#
 
 lcdmaker=$(grep -c "lcd_maker_id=1" /proc/cmdline)
 if [ $lcdmaker == 1 ]; then
 	echo "JDI panel detected"
-	find /tmp/boot.img -type f -exec sed -i 's/console=ttyHSL0,115200,n8 androidboot.hardware=g2 androidboot.bootdevice=msm_sdcc.1 user_debug=31 msm_rtb.filter=0x0 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd androidboot.selinux=permissive/console=ttyHSL0,115200,n8 androidboot.hardware=g2 androidboot.bootdevice=msm_sdcc.1 user_debug=31 msm_rtb.filter=0x0 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd androidboot.selinux=permissive/g' {} \;
+	find /tmp/loki/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/g' {} \;
+	find /tmp/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/g' {} \;
 elif [ $lcdmaker == "0" ]; then
 	echo "LGD panel detected"
-	find /tmp/boot.img -type f -exec sed -i 's/console=ttyHSL0,115200,n8 androidboot.hardware=g2 androidboot.bootdevice=msm_sdcc.1 user_debug=31 msm_rtb.filter=0x0 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd androidboot.selinux=permissive/console=ttyHSL0,115200,n8 androidboot.hardware=g2 androidboot.bootdevice=msm_sdcc.1 user_debug=31 msm_rtb.filter=0x0 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd androidboot.selinux=permissive/g' {} \;
+	find /tmp/loki/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/g' {} \;
+	find /tmp/boot.img -type f -exec sed -i 's/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_jdi_cmd/mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_g2_lgd_cmd/g' {} \;
 else
 	echo "lcd_maker_id doesn't exist. Something went wrong."
 fi
