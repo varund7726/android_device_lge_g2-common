37b88176507a8a9ce6f77e140ad52a8b78e2347c
Rashed Abdel-Tawab <rashed@linux.com>
2015-02-12 19:53:46 -0500

    g2: nfc: Remove nfcee_access_debug, use release version
    Change-Id: I6cb6ac35271a245198c0cbdc8bb6e584e673b7c9

--- g2.mk
+++ g2.mk
@@ -158,16 +158,9 @@ PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     NfcNci
 
-# NFCEE access control
-ifeq ($(TARGET_BUILD_VARIANT),user)
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/nfc/nfcee_access.xml
-else
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/nfc/nfcee_access_debug.xml
-endif
-
 # NFC access control + feature files + configuration
 PRODUCT_COPY_FILES += \
-    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml \
+    $(LOCAL_PATH)/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
 
