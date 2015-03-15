5e1e5d0b39e376ec191d4e34fcb4674083686d5f
Rashed Abdel-Tawab <rashed@linux.com>
2015-03-14 22:01:35 -0400

    Revert "g2: releasetools: Replace loki with bump"
    This reverts commit be8485e257d1c77146bea02bf824bd9d39fe17b3.

--- g2.mk
+++ g2.mk
@@ -184,8 +184,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/bcmdhd.cal:system/etc/wifi/bcmdhd.cal
 
-# Panel detection script
+# This hw ships locked, work around it with loki
 PRODUCT_PACKAGES += \
+    loki.sh \
+    loki_tool_static_g2 \
+    recovery-transform.sh \
     panel.sh
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
